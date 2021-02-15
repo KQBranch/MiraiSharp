c3025879d69eff0c18534663d7a08534ca0abcd9
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-02-15 11:05:27 +0000

    lib/net: add initialize for httpClient

--- MiraiSharp.Library/Net/HttpOperation.cs
+++ MiraiSharp.Library/Net/HttpOperation.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace MiraiSharp.Library.Net
 {
     class HttpOperation
     {
+        public static void Initialize()
+        {
+            _httpClient = new HttpClient()
+            {
+                Timeout = TimeSpan.FromSeconds(5),
+            };
+            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                "Chrome/88.0.4324.150 Safari/537.36");
+        }
+
+        private static HttpClient _httpClient;
+
         [Obsolete("Have Not Implemented！")]
         public static string Get(string url)
         {
